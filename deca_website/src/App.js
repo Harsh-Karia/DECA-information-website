1bbd2f188d19cd65dc8041286a6fc4afed8b6550
sohamk <sohamk0524@gmail.com>
2024-03-06 16:14:31 -0800

    added router to website and different pages

--- deca_website/src/App.js
+++ deca_website/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Apply from './Pages/Apply';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+
+        <Navbar/>
+        <Routes>
+          <Route path='/about' element= {<About/>}/>
+          <Route path='/contact' element= {<Contact/>}/>
+          <Route path='/apply' element= {<Apply/>}/>
+        </Routes>
+
+      </Container>
     </div>
-  );
+
+    );
 }
 
 export default App;

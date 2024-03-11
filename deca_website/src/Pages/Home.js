4c1b2e75d2e6da85fa185d768d4b76ef353fa693
Michelle <michellew.yeoh@gmail.com>
2024-03-11 11:22:50 -0700

    what is deca

--- deca_website/src/Pages/Home.js
+++ deca_website/src/Pages/Home.js
@@ -9,9 +9,9 @@ export default function Home(){
 
 
 			</div>
-			<div>
+			<div style={{backgroundColor: 'lightGrey', paddingLeft: '200px',paddingRight: '200px', paddingTop: '50px', paddingBottom: '50px'}}>
 				<h2>What is DECA</h2>
-				<div style={{paddingLeft: '200px',paddingRight: '200px'}}>
+				<div>
 				<p>DECA stands for Distributive Education Clubs of America, an association that encourages the development of business and leadership skills through academic conferences and competitions
 An organization that prepares emerging leaders and entrepreneurs for careers in Marketing, Finance, Hospitality, and Management worldwide.</p>
 				</div>

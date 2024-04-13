e80ea75ebeed28099bfcad90b5744e3378ed1be3
Michelle <michellew.yeoh@gmail.com>
2024-04-12 17:34:46 -0700

    home format

--- deca_website/src/Pages/Apply.js
+++ deca_website/src/Pages/Apply.js
@@ -1,8 +1,17 @@
 import React from "react";
 import '../Styles/Pages.css';
+import {Link} from 'react-router-dom';
+import ig_logo from '../Images/ig_logo.png'
 
 export default function Apply(){
 	return(
-		<h1>Apply Page</h1>
+		<div>
+			<h1>Apply Page</h1>
+			<p>Check Back in Fall!</p>
+			<p>Stay updated with our socials:</p>
+			<Link target={"_blank"} to="https://www.instagram.com/deca.ucd/">
+                <img src={ig_logo} alt="ig_logo" style={{height:50}}/>
+            </Link>
+		</div>
 	)
 }
--- deca_website/src/Pages/Home.js
+++ deca_website/src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../Styles/Pages.css';
-import deca_logo from '../Images/deca_logo.png'
+// import deca_logo from '../Images/deca_logo.png'
 import deca_table from '../Images/deca_table.jpg'
 import cookie_deco from '../Images/cookie_deco.png'
 import pizza_social from '../Images/pizza_social.jpg'
@@ -12,40 +12,32 @@ export default function Home(){
 	return(
 		<div>	
 			<br></br>
-			<div className='box' >
-				<div className="leftBox">
-					<h1>DECA at UC Davis</h1>
-					<p>DECA at UC Davis is a student organization that prepares emerging leaders in marketing, finance, hospitality, and management through case challenges, professional development, and networking opportunities.</p>
+			<div class="home_front"style={{color:'grey'}}>
+				<div>
+					<h1>Deca at UC Davis</h1>
+					<h3>Preparing emerging leaders in marketing, finance, hospitality, and management at UC Davis</h3>
+					<Link to="/about" >
+                            <button class = "rbutton">Learn More About Deca at UC Davis</button>
+                    </Link>
+					<br></br>
+					<img src={deca_table} alt="deca_table" width='400px'style={{margin:'50px'}}/>
 				</div>
-				<div className="rightBox">
+				{/* <div className="rightBox">
 					<img src={deca_logo} alt="deca_logo" width='300px'/>
-				</div>
+				</div> */}
 			</div>
 
 
 			<br></br>
 			<div className='box' >
 				<div className="leftBox">
-					<img src={deca_table} alt="deca_table" width='300px'/>
-					<p>about</p>
-					<Link to="/about">
-                            <button class = "rbutton">Learn More</button>
-                    </Link>
-					<div style={{padding:'20px'}}></div>
-					<img src={club_meeting} alt="club_meeting" width='300px'/>
-					<p>contact</p>
-					<Link to="/contact">
-                            <button class = "rbutton">Contact Us</button>
-                    </Link>
-				</div>
-
-				<div className="rightBox">
 					<img src={cookie_deco} alt="cookie_deco" width='300px'/>
 					<p>events</p>
 					<Link to="/events">
                             <button class = "rbutton">Explore Opportunities</button>
                     </Link>
-					<div style={{padding:'20px'}}></div>
+				</div>
+				<div className="rightBox">
 					<img src={pizza_social} alt="pizza_social" width='300px'/>
 					<p>apply</p>
 					<Link to="/apply">
@@ -59,7 +51,7 @@ export default function Home(){
 					<h1>Connect with Us.</h1>
 					<h3>Contact DECA at UC Davis to learn more about future opporutnites.</h3>
 					<Link to="/apply">
-                            <button>Contact Us</button>
+                            <button class = "rbutton">Contact Us</button>
                     </Link>
 				</div>
 				<div className="rightBox"> 
